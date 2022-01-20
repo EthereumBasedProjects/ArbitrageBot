46fbd3846a82f12d122d56da411333022f8e8a55
ysccoding <richard.has.yan2@gmail.com>
2022-01-20 10:46:50 -0800

    Mainnet Addressses for ERC20 Arbitrage Tokens

--- scripts/arbitrageBot.js
+++ scripts/arbitrageBot.js
@@ -19,3 +19,11 @@ const providerURL = 'http://localhost:8545';
 const provider = new ethers.providers.JsonRpcProvider( providerURL );
 const localTestAccountAddress = '';
 const privateKey = '';
+
+/* Smart Contract Address for Mainnet ERC20 Tokens */
+const Tokens = {
+    WETH: WETH\[ ChainId.MAINNET \],
+    DAI: new Token( ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18 , 'DAI'),
+    BAT: new Token( ChainId.MAINNET, '0x2E642b8D59B45a1D8c5aEf716A84FF44ea665914', 18, 'BAT') ,
+    MKR : new Token ( ChainId.MAINNET, '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2', 18, 'MKR')
+};
